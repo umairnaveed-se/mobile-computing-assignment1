466c43fbc166f77405a11c54955bc3c03e87c6c9
umairnaveed-se <umair_chishti98@hotmail.com>
2021-10-10 22:03:34 +0500

    edited index and added js

---
+++ js/swipe.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".swiper-container", {
+    freeMode: true,
+    effect: "coverflow",
+    direction: "horizontal",
+    cssMode: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    mousewheel: {
+        releaseOnEdges: true
+    },
+    /* navigation:{
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }, */
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 350,
+      modifier: 1,
+      slideShadows: true
+    },
+    pagination: {
+      el: ".swiper-pagination"
+    }
+});
